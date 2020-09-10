2987fb1fe88bb208d6ff77fad6c6c12859fbef3f
Gridech <64532051+Gridech@users.noreply.github.com>
2020-09-10 16:37:12 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,8 +4,43 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
+  // useEffect <<<<<<< POWERFUL
+  // Piece of code which runs based on a given condition
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // the user is logged in...
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out...
+
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+
+    return () => {
+      // Any cleanup operations go in here...
+      unsubscribe();
+    };
+  }, []);
+
+  console.log("USER IS >>> ", user);
+
   return (
     <Router>
       <div className="app">
@@ -13,6 +48,9 @@ function App() {
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
           <Route path="/">
             <Header />
--- src/Checkout.js
+++ src/Checkout.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { useStateValue } from "./StateProvider";
-import CheckoutProduct from "./CheckoutProduct";
 import "./Checkout.css";
 import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
-  const [{ basket }] = useStateValue();
-
+  const [{ basket, user }, dispatch] = useStateValue();
+  let userName = user ? user.email.split("@") : [];
+  let username = user ? userName[0] : "Guest";
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -15,30 +16,26 @@ function Checkout() {
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
-        {basket?.length === 0 ? (
-          <div>
-            <h2>Your Shopping Basket is empty</h2>
-            <p>
-              You have no items in your basket. To buy one or more items, click
-              "Add to basket" next to the item.
-            </p>
-          </div>
-        ) : (
-          <div>
-            <h2 className="checkout__title">Your Shopping Basket</h2>
-
-            {/* List out all of the Checkout Products */}
-            {basket?.map((item) => (
+        <div>
+          <h3 className="checkout__user">Hello, {username}</h3>
+          <h2 className="checkout__title">
+            {basket?.length > 0
+              ? "Your Shopping Basket"
+              : "Your Shopping Basket is Empty"}
+          </h2>
+          {basket.map(({ id, title, image, price, rating }, index) => {
+            return (
               <CheckoutProduct
-                id={item.id}
-                title={item.title}
-                image={item.image}
-                price={item.price}
-                rating={item.rating}
+                key={index}
+                id={id}
+                title={title}
+                price={price}
+                rating={rating}
+                image={image}
               />
-            ))}
-          </div>
-        )}
+            );
+          })}
+        </div>
       </div>
       {basket.length > 0 && (
         <div className="checkout__right">
